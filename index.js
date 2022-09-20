aa3ba10426cfb362ed9d8daadb1748c44102bc35
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-20 23:25:48 +0300

    feat: add variables in place of place holders in template-product.html

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
+// const fs = require("fs");
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 //blocking, synchronus code
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textout = `This is what we know about the ovacado: ${textIn}`;
-fs.writeFileSync("./txt/output.txt", textout);
+// const textout = `This is what we know about the ovacado: ${textIn}`;
+// fs.writeFileSync("./txt/output.txt", textout);
 
-//Non blocking asynchronus code
+// //Non blocking asynchronus code
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) {
+//     return console.log("An error occured");
+//   }
 
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File has been written");
-      });
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("File has been written");
+//       });
+//     });
+//   });
+// });
+
+// '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+// '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+//Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObj = JSON.parse(data);
+
+const Server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text-html",
+      "my-own-header": "hello world",
     });
-  });
+
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+Server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
 });
